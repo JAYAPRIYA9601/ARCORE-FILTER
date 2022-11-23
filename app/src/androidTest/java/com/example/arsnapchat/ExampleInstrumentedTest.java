8a415900c3d6ef5195fbcc112bbaf5fa816d92bf
JAYAPRIYA JP <jayapriya9601@gmail.com>
2022-11-23 19:46:22 +0530

    ARCORE FILTER APP

---
+++ app/src/androidTest/java/com/example/arsnapchat/ExampleInstrumentedTest.java
@@ -0,0 +1,23 @@
+package com.example.arsnapchat;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+//https://github.com/JAYAPRIYA9601/ARcore-filter.git
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.arsnapchat", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/com/example/arsnapchat/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.arsnapchat;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * https://github.com/JAYAPRIYA9601/ARcore-filter.git
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
